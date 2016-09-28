3e7c665eeade5e98e15e58d98bd06aea9eb289b5
Mohab <mohabinventor@yahoo.com>
2016-09-28 15:45:14 +0200

    'Database.java_should_work'

--- WEB-INF/java/Database.java
+++ WEB-INF/java/Database.java
@@ -10,9 +10,9 @@ public class Database{
     Statement stm = null;
     public Database(){
         try{
-	    Class.forName(DRIVER);
-	    con = DriverManager.getConnection(URL, NAME, PASS);
-	    stm = con.createStatement();
+            Class.forName(DRIVER);
+            con = DriverManager.getConnection(URL, NAME, PASS);
+            stm = con.createStatement();
             stm.execute("create database if not exists sfp;");
             URL += "sfp";
             con = DriverManager.getConnection(URL, NAME, PASS);
@@ -20,77 +20,75 @@ public class Database{
             DatabaseMetaData meta = con.getMetaData();
             ResultSet tables = meta.getColumns(null, null, "userinfo", null);
             String userInfoTable = "create table userinfo ( "
-            	+ "ID INT PRIMARY KEY AUTO_INCREMENT, "
-            	+ "NAME VARCHAR(10), "
-            	+ "PASS VARCHAR(10), "
-            	+ "IPNUMS TINYINT);";
+                + "ID INT PRIMARY KEY AUTO_INCREMENT, "
+                + "NAME VARCHAR(10), "
+                + "PASS VARCHAR(10), "
+                + "IPNUMS TINYINT);";
             String userIPsTable = "craete table userips ( "
-            	+ "ID INT, "
-            	+ "IP VARCHAR(15), "
-            	+ "ACTIVE BOOLEAN, " //BOOLEAN IS ALIAS FOR BIT(1)
-            	+ "DES VARCHAR(255));";
+                + "ID INT, "
+                + "IP VARCHAR(15), "
+                + "ACTIVE BOOLEAN, " //BOOLEAN IS ALIAS FOR BIT(1)
+                + "DES VARCHAR(255));";
             if(!tables.next()) {
-            	stm.execute(userInfoTable);
-            	stm.execute(userIPsTable);
+                stm.execute(userInfoTable);
+                stm.execute(userIPsTable);
             }
-	}catch(Exception e) {
-            e.printStackTrace();  
-        }
+        }catch(Exception e) { e.printStackTrace();  }
     }
     public void insertIP(int id, String ip, String des){
         PreparedStatement ps = null;
-	try{
-	    ps = con.prepareStatement("insert into userips (ID, IP, ACTIVE, DES) values(?, ?, ?, ?)");
-	    ps.setInt(1, id);
-	    ps.setString(2, ip);
+        try{
+            ps = con.prepareStatement("insert into userips (ID, IP, ACTIVE, DES) values(?, ?, ?, ?)");
+            ps.setInt(1, id);
+            ps.setString(2, ip);
             ps.setBoolean(3, true);
-	    ps.setString(4, des);
-	    ps.execute();
-	}catch(Exception e) { e.printStackTrace();  }
+            ps.setString(4, des);
+            ps.execute();
+        }catch(Exception e) { e.printStackTrace();  }
     }
 	// sign up from website.
     public void signUp(String name, String pass){ 
         try{//TODO limit num of signup per day.
-	    stm.execute("insert into userinfo (NAME, PASS) values ("+name+", "+pass+");");
-	}catch(Exception e) { e.printStackTrace();  }
+            stm.execute("insert into userinfo (NAME, PASS) values ("+name+", "+pass+");");
+        }catch(Exception e) { e.printStackTrace();  }
     }
-	// sign up from android client.
-	public void signUP(String name, String pass, String ip, String des){
-		int id = getAuthID(name, pass);//TODO can i return id from insert statement?
-		if(id >= 0){//user is registered from browser client
+    // sign up from android client.
+    public void signUP(String name, String pass, String ip, String des){
+        int id = getAuthID(name, pass);//TODO can i return id from insert statement?
+        if(id >= 0){//user is registered from browser client
             insertIP(id, ip, des);
             return;
-		}
-		try{//TODO limit num of signup per day.
-			stm.execute("insert into userinfo (NAME, PASS, IPNUMS) values ("+name+", "+pass+", "+1+");");
-		}catch(Exception e) { e.printStackTrace();  }
-		insertIP(id, ip, des);
-	}
-	public int getAuthID(String name, String pass){
-		try{
-			ResultSet rs = stm.executeQuery("select ID from userinfo where userinfo.NAME="+NAME+" AND us.PASS = "+pass+";");
-		    rs.next();
-		    return rs.getInt(1);
-		}catch(Exception e) { e.printStackTrace(); }
-		return -1;
-	}
-	public int getIPsNum(int id){
-		try{
-			ResultSet rs = stm.executeQuery("select IPNUMS from userinfo where userinfo.ID = (select ID from userips where userips.IP = "+id+");");
-			rs.next();
-			return rs.getInt(1);
-		}catch(Exception e) { e.printStackTrace();  }
-		return 0;
-	}
-	public HashMap getUserActiveIPs(int id){
-		try{
-			HashMap<String, String> hm = new HashMap<String, String>();
-			ResultSet rs = stm.executeQuery("select IP, DES from userips where ID = "+id+";");
-			while(rs.next()){
-				hm.put(rs.getString(1), rs.getString(2));
-			}
-			return hm;
-		}catch(Exception e) { e.printStackTrace();  }
-		return null;
-	}
+        }
+        try{//TODO limit num of signup per day.
+            stm.execute("insert into userinfo (NAME, PASS, IPNUMS) values ("+name+", "+pass+", "+1+");");
+        }catch(Exception e) { e.printStackTrace();  }
+        insertIP(id, ip, des);
+    }
+    public int getAuthID(String name, String pass){
+        try{
+            ResultSet rs = stm.executeQuery("select ID from userinfo where NAME=\""+name+"\" AND PASS = \""+pass+"\";");
+            rs.next();
+            return rs.getInt(1);
+        }catch(Exception e) { e.printStackTrace(); }
+        return -1;
+    }
+    public int getIPsNum(int id){
+        try{
+            ResultSet rs = stm.executeQuery("select IPNUMS from userinfo where ID = (select ID from userips where userips.IP = \""+id+"\");");
+            rs.next();
+            return rs.getInt(1);
+        }catch(Exception e) { e.printStackTrace();  }
+        return 0;
+    }
+    public HashMap getUserActiveIPs(int id){
+        try{
+            HashMap<String, String> hm = new HashMap<String, String>();
+            ResultSet rs = stm.executeQuery("select IP, DES from userips where ID = "+id+";");
+            while(rs.next()){
+                hm.put(rs.getString(1), rs.getString(2));
+            }
+            return hm;
+        }catch(Exception e) { e.printStackTrace();  }
+        return null;
+    }
 }
